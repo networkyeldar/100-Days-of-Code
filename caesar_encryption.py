ff2c773011482c3af85c726ac7663141e926434f
networkyeldar <100433043+networkyeldar@users.noreply.github.com>
2023-09-27 17:09:57 +0600

    Update caesar_encryption.py

--- caesar_encryption.py
+++ caesar_encryption.py
@@ -1,7 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-
 def caesar(plain_text, shift_amount,direct):
   message = ""
   if direct == "encode":
