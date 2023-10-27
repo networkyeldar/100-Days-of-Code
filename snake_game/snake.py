406821451278755165c1d65b1757b5e81b5f8fe2
networkyeldar <100433043+networkyeldar@users.noreply.github.com>
2023-10-27 18:14:32 +0600

    Create snake.py

---
+++ snake_game/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+class Snake:
+
+    def __init__(self):
+        self.all_segments = []
+        self.create_snake()
+        self.head = self.all_segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.all_segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.all_segments[-1].position())
+    def move_snake(self):
+        for seg_num in range(len(self.all_segments) - 1, 0, -1):
+            new_x = self.all_segments[seg_num - 1].xcor()
+            new_y = self.all_segments[seg_num - 1].ycor()
+            self.all_segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def move_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def move_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def move_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def move_right(self):
+        if self.head.heading() != LEFT:
+            self.all_segments[0].setheading(RIGHT)
