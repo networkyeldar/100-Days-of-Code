b1bc501522daa3b13e94cfa651981d3bf46edf8d
networkyeldar <100433043+networkyeldar@users.noreply.github.com>
2023-11-02 17:30:46 +0600

    Update scoreboard.py

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,20 +1,31 @@
 from turtle import Turtle
 
+
+
+
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.goto(-50,260)
+        with open("my_file.txt") as file:
+            self.highscore = int(file.read())
+
+        self.goto(-100,260)
         self.color("white")
-        self.write(f"Score: {self.score}", font=("Arial", 14, "normal"))
+        self.write(f"Score: {self.score}  High Score: {self.highscore}", font=("Arial", 14, "normal"))
         self.hideturtle()
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", font=("Arial", 14, "normal"))
+        self.write(f"Score: {self.score}  High Score: {self.highscore}", font=("Arial", 14, "normal"))
 
-    def game_over(self):
-        self.penup()
-        self.goto(-50, 0)
-        self.write(f"Gameover!", font=("Arial", 20, "normal"))
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("my_file.txt", mode="w") as file:
+                file.write(f"{self.highscore}")
+        self.score = 0
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.highscore}", font=("Arial", 14, "normal"))
+
